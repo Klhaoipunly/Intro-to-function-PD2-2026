2ac06667b68f912083a2c4b5d5a1411901b27673
Klhaoipunly <junze1212jl@icloud.com>
2026-09-18 07:13:18 -0400

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,16 +1,45 @@
 import turtle
 from turtle import *
 t = Turtle()
+t.speed(0)
 t.shape('turtle')
-turtle.speed(10000)
 
-for i in range(4):
-    t.forward(100)
-    t.left(90)
+#def square():
+#   for i in range(4):
+#       t.forward(100)
+#       t.left(90)
 
-def square():
-    #i is an incrementor/iterator
-    for i in range(60):
-        t.right(y)
-t.right(5)
+#for i in range(60):
+#   square()
+#   t.right(5)
+
+#def square(x,y):
+   #for i in range(4):
+       #t.forward(x)
+       #t.right(y)
+
+#def square2():
+    #length = 5
+    #for i in range(60):
+        #square(length,90)
+        #t.right(5)
+        #length += 5
+
+#square2()
+
+def star(length):
+    for i in range(5):
+        t.forward(length)
+        t.right(144)
+
+def star2():
+    length = 10
+
+    for i in range(50):
+        star(length)
+        t.right(6)
+        length +=6
+
+star2()
+
 turtle.done()
